f89a65fb0cf33ac566199e8ba735fee30d9d19eb
Waylon Bader <wbader@hawaii.edu>
2022-04-14 16:11:46 -0400

    Added config.h to have expressive error messaging

---
+++ config.h
@@ -0,0 +1,13 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  fatCat - EE 205 - Spr 2022
+///
+/// @file config.h
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+#define PROGRAM_NAME "Fat Cat"
