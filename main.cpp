f157f6aa6b01ae804779400eebf6a38f33fdc6b0
Waylon Bader <wbader@hawaii.edu>
2022-04-14 14:00:08 -0400

    Created main.cpp, test harness for class Weight

---
+++ main.cpp
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file main.cpp
+/// @version 1.0
+///
+/// This is a test harness for the fatCat lab to test the Weight Class.
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   17_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+
+int main(){
+
+}
