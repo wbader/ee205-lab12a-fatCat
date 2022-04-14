b345f4b9e4d23478bf5210fd656d1d6a85fb8823
Waylon Bader <wbader@hawaii.edu>
2022-04-14 14:06:43 -0400

    Created Weight.cpp/.h

---
+++ Weight.cpp
@@ -0,0 +1,11 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.cpp
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   14_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#include "Weight.h"
---
+++ Weight.h
@@ -0,0 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205-lab12a-fatCat - EE 205 - Spr 2022
+///
+/// @file Weight.h
+/// @version 1.0
+///
+/// @author Waylon Bader <wbader@hawaii.edu>
+/// @date   14_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#ifndef EE205_LAB12A_FATCAT_WEIGHT_H
+#define EE205_LAB12A_FATCAT_WEIGHT_H
+
+
+class Weight {
+
+};
+
+
+#endif //EE205_LAB12A_FATCAT_WEIGHT_H
